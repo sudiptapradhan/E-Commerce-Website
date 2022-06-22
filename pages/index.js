601645294a19b9c4d8cb761e9b90f75191699705
Sudipta Pradhan <64891121+sudiptapradhan@users.noreply.github.com>
2022-06-23 00:43:09 +0530

    added products and footer-banner

--- pages/index.js
+++ pages/index.js
@@ -18,17 +18,16 @@ const Home = ({products, bannerData}) => (
     </div>
 
     <div className='products-container'>
-      {products?.map((product) => product.name )
+      {products?.map((product) => <Product key = {product._id} product={product}/> )
       }
     </div>
 
-    <FooterBanner/>
+    <FooterBanner footerBanner = {bannerData && bannerData[0]}/>
     </div>
   );
 
   export const getServerSideProps = async () => {
-    //a sanity query which says lets grab all the products from our sanity dashboard
-    
+    //a sanity query which says lets grab all the products from our sanity dashboard 
     //whatever gets inside getServerSideProps that gets populated inside our function
     const query = `*[_type == "product"]`;
     const products = await client.fetch(query);
