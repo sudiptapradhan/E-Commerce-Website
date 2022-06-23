3ad46124d434eb9478baa9fc550cfff08480a80a
Sudipta Pradhan <64891121+sudiptapradhan@users.noreply.github.com>
2022-06-23 12:10:50 +0530

    added navbar and footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import {AiOutlineShopping} from 'react-icons/ai';
 
+//the button is going to open up the cart
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">E-Comm Store</Link>
+      </p>
+      
+      <button type='button' className='cart-icon' onClick="">
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
